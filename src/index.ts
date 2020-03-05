89c4de7cdd4de60077f4c8d1b48dcd6b35f2024f
Alexander Dransfield <alex@futr.ai>
2020-03-05 23:30:36 +0000

    Starting on env port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { server } from './server';
 import { logger } from './lib/logger';
 import { ServerInfo } from 'apollo-server';
 
-server.listen().then(({ url }: ServerInfo) => {
+const port = process.env.PORT || 4000;
+
+server.listen(port).then(({ url }: ServerInfo) => {
   logger.info('Server ready at', url);
 });
