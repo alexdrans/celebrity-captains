a95acb2660f82c38507cba9e6c5beb56976a9467
Alexander Dransfield <alex@futr.ai>
2020-03-05 23:35:45 +0000

    Db connection env

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,15 @@
 import { Pool } from 'pg';
 
-const pool: Pool = new Pool({
-  host: 'postgres',
-  user: 'postgres',
-  database: 'postgres',
-});
+const connectionOptions = process.env.DATABASE_URL
+  ? {
+      connectionString: process.env.DATABASE_URL,
+    }
+  : {
+      host: 'postgres',
+      user: 'postgres',
+      database: 'postgres',
+    };
+
+const pool: Pool = new Pool(connectionOptions);
 
 export const connection = pool;
